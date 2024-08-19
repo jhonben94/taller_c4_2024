24e1deee56d871f24d73a5640a66b14372315b32
jhony <jhonyben.94@gmail.com>
2024-08-19 19:31:08 -0400

    Clase anterior

--- src/main/java/py/edu/ucom/GreetingResource.java
+++ src/main/java/py/edu/ucom/GreetingResource.java
@@ -11,6 +11,13 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Taller 1, se modifica recurso /hello";
+    }
+
+    @GET
+    @Path("/path2")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello2() {
+        return "Taller 2, se modifica recurso /hello/test2";
     }
 }
